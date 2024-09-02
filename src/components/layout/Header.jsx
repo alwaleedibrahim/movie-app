e835e1837fc54a8d012021637773f181a048fb47
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2024-09-02 10:23:05 +0300

    STYLE: Add Dark and Light Modes

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
-import "./Header.css"
-
+import "./Header.css";
+import { Button } from "react-bootstrap";
 
 export default function Header() {
+  const [theme, setTheme] = useState('dark');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-bs-theme', theme);
+  }, [theme]);
+
   return (
     <Container fluid className="p-0">
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar>
         <Container fluid>
-          <NavLink to="/" className={["navbar-brand"]}>Movie App</NavLink>
+          <NavLink to="/" className={["navbar-brand"]}>
+            Movie App
+          </NavLink>
           <Nav className="me-auto">
-            <NavLink to="/" className={["nav-link"]}>Home</NavLink>
-            <NavLink to="/movies" className={["nav-link"]}>Movies</NavLink>
+            <NavLink to="/" className={["nav-link"]}>
+              Home
+            </NavLink>
+            <NavLink to="/movies" className={["nav-link"]}>
+              Movies
+            </NavLink>
           </Nav>
+          <Button variant={theme == 'light' ? "outline-dark" :"outline-info"} onClick={toggleTheme}>
+            {theme == 'light' ? <>&#127769;</> : <>&#9788;</>}
+          </Button>
         </Container>
       </Navbar>
     </Container>
